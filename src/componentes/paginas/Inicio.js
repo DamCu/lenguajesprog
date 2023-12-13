3cab135e9ca245a94a780e10d85698f683b79812
DamCu <107067947+DamCu@users.noreply.github.com>
2023-12-13 05:25:27 -0400

    Add files via upload

---
+++ src/componentes/paginas/Inicio.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Carousel from '../carousel/Carousel'
+import Footer from '../footer/Footer'
+
+
+const inicio = () => {
+  return (
+    <div>
+        <Carousel/>
+        <center>
+        <br></br>
+        <br></br>
+        <h1>LOS LENGUAJES DE PROGRAMACIÓN</h1>
+        <br></br>
+        </center>
+        <div>
+      
+      <p>
+        En el fascinante mundo de la programación, los lenguajes de
+        programación son los cimientos sobre los cuales se construyen las
+        innovaciones tecnológicas. Cada lenguaje tiene su propia historia,
+        características distintivas y aplicaciones específicas que han dejado
+        una huella única en la evolución de la informática y la programación.
+      </p>
+      <p>
+        Esta página está diseñada como una guía exhaustiva para explorar el
+        vasto panorama de lenguajes de programación. Ya sea que seas un
+        principiante entusiasmado por aprender a programar o un desarrollador
+        experimentado en busca de nuevas perspectivas, aquí encontrarás
+        información detallada sobre diversos lenguajes que han contribuido
+        significativamente al mundo de la tecnología.
+      </p>
+      <p>
+        A través de secciones dedicadas, explorarás la historia fascinante de
+        cada lenguaje, sus características distintivas, aplicaciones prácticas y
+        ejemplos de código que te ayudarán a comprender mejor su sintaxis y
+        funcionalidades.
+      </p>
+      <p>
+        ¡Adéntrate en el emocionante viaje de descubrimiento que ofrece esta
+        página y amplía tu conocimiento sobre los pilares que sustentan el
+        vasto universo de la programación!
+      </p>
+    </div>
+
+        
+
+        <Footer/>
+    </div>
+  )
+}
+
+export default inicio
